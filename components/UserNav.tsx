5fd66cd48f899b384b87152db3911be0a09de858
nikolas <nikolas.bitencourt@4mti.com.br>
2024-02-28 18:04:41 -0300

    att

--- components/UserNav.tsx
+++ components/UserNav.tsx
@@ -1,6 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
-import { DropdownMenu, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const UserNav = () => {
   return (
@@ -8,11 +12,17 @@ const UserNav = () => {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-10 w-10 rounded-full">
           <Avatar className="h-10 w-10 rounded-full">
-            <AvatarImage /* src="https://github.com/shadcn.png"  />
+            <AvatarImage /* src="https://github.com/shadcn.png" */ />
             <AvatarFallback>Jan</AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
+
+      <DropdownMenuContent
+        className="w-56"
+        align="end"
+        forceMount
+      ></DropdownMenuContent>
     </DropdownMenu>
   );
 };
